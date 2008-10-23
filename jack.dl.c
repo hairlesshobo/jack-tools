1a34ac98186a6324d91673a5b3c8e5392149c9a9
Rohan Drape <rd@slavepianos.org>
2008-10-23 01:39:06 +0000

    fixes to jack.dl

--- jack.dl.c
+++ jack.dl.c
@@ -4,19 +4,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <time.h>
 #include <dlfcn.h>
 
 #include <jack/jack.h>
 #include <jack/thread.h>
 #include <lo/lo.h>
 
+#include "common/jack-port.h"
 #include "jack.dl.h"
 
 int dsp_run(jack_nframes_t nf, void *ptr)
 {
   struct world *w = (struct world *)ptr;
-  int i, j;
-  for(i = 0; i < w->nc; i++) {
+  for(int i = 0; i < w->nc; i++) {
     w->out[i] = (float *)jack_port_get_buffer(w->op[i], nf);
   }
   w->dsp_step(w, w->st, nf);
@@ -43,52 +44,46 @@ int osc_quit(const char *p, const char *t, lo_arg **a, int n, void *d, void *u)
   return 0;
 }
 
+void fail(char *s)
+{
+  fprintf(stderr, s);
+  exit(EXIT_FAILURE);
+}
+
+void world_init(struct world *w, int nc)
+{
+  w->nc = nc;
+  w->ip = malloc(w->nc * sizeof(jack_port_t *));
+  w->op = malloc(w->nc * sizeof(jack_port_t *));
+  w->in = malloc(w->nc * sizeof(float *));
+  w->out = malloc(w->nc * sizeof(float *));
+  w->ef = false;
+  w->c = jack_client_new("jack.dl");
+  if(!w->c) fail("jack.dl: could not create jack client\n");
+  jack_set_process_callback(w->c, dsp_run, w);
+  w->sr = (float)jack_get_sample_rate(w->c);
+  jack_port_make_standard(w->c, w->ip, w->nc, false);
+  jack_port_make_standard(w->c, w->op, w->nc, true);
+}
+
 int main(int argc, char **argv)
 {
   struct world w;
-  int i;
   void *h;
   lo_server_thread osc;
   h = dlopen(argv[1], RTLD_LAZY);
-  if(!h) {
-    fprintf(stderr, "dlopen failed: %s\n", dlerror());
-    return EXIT_FAILURE;
-  }
+  if(!h) fail(dlerror());
   w.dsp_init = dlsym(h, "dsp_init");
   w.dsp_step = dlsym(h, "dsp_step");
-  w.nc = 2;
-  w.ef = false;
-  w.c = jack_client_new("jack.dl");
-  if(!w.c) {
-    fprintf(stderr, "jack.dl: could not create jack client\n");
-    return EXIT_FAILURE;
-  }
-  jack_set_process_callback(w.c, dsp_run, &w);
-  w.sr = (float)jack_get_sample_rate(w.c);
-  for(i = 0; i < w.nc; i++) {
-    char name[64];
-    snprintf(name, 64, "out_%d", i + 1);    
-    w.op[i] = jack_port_register(w.c, 
-                                 name,
-                                 JACK_DEFAULT_AUDIO_TYPE, 
-                                 JackPortIsOutput, 
-                                 0);
-    if(!w.op[i]) {
-      fprintf(stderr, "jack.dl: could not create jack output port\n");
-      return EXIT_FAILURE;
-    }
-  }
+  world_init(&w, 8);
   osc = lo_server_thread_new("57190", osc_error);
   lo_server_thread_add_method(osc, "/c_set", "if", osc_cset, &w);
   lo_server_thread_add_method(osc, "/quit", NULL, osc_quit, &w);
   lo_server_thread_start(osc);
-  if(jack_activate(w.c)) {
-    fprintf(stderr, "jack.dl: jack_activate() failed\n");
-    return EXIT_FAILURE;
-  }
+  if(jack_activate(w.c)) fail("jack.dl: jack_activate() failed\n");
   w.st = w.dsp_init(&w);
   while(!w.ef) {
-    struct timespec t = {1, 0};
+    struct timespec t = {0, 100000000};
     nanosleep(&t, NULL);
   }
   jack_client_close(w.c);
--- jack.dl.h
+++ jack.dl.h
@@ -1,23 +1,23 @@
+#include <stdbool.h>
 #include <jack/jack.h>
-
-#define MAX_NC 2
 
 struct world {
   void* (*dsp_init)(struct world *);
   void (*dsp_step)(struct world *, void *, int);
   jack_client_t *c;             /* client */
-  jack_port_t *op[MAX_NC];      /* output ports */
+  jack_port_t **ip;             /* input ports */
+  jack_port_t **op;             /* output ports */
   int nc;                       /* number of channels */
   float sr;                     /* sample rate */
-  float *in[MAX_NC];            /* input data */
-  float *out[MAX_NC];           /* output data */
+  float **in;                   /* input data */
+  float **out;                  /* output data */
   float ctl[64];                /* control data */
   void *st;                     /* state */
-  int ef;                       /* exit flag */
+  bool ef;                      /* exit flag */
 };
 
 #define w_sr(w) (w)->sr
 #define w_cget(w,i) (w)->ctl[(i)]
 #define w_cset(w,i,n) (w)->ctl[(i)]=(n)
 #define w_out1(w,i,n) (w)->out[0][(i)]=(n)
-#define w_out2(w,i,n1,n2) {(w)->out[0][(i)]=(n1);(w)->out[1][(i)]=(n1);}
+#define w_out2(w,i,n1,n2) {(w)->out[0][(i)]=(n1);(w)->out[1][(i)]=(n2);}
