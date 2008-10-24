de6ac07f6b81719a907888100965571b40aec9b2
Rohan Drape <rd@slavepianos.org>
2008-10-24 02:00:38 +0000

    make help/sin.c interpolate control values

--- help/sin.c
+++ help/sin.c
@@ -6,7 +6,7 @@
 
 struct sinosc {
   float phase;                  /* oscillator phase */
-  float incr;                   /* osillator increment */
+  float f, a, p;                /* frequency, amplitude, pan */
 }; 
 
 #define TWO_PI (2 * M_PI)
@@ -26,11 +26,14 @@ float step_phasor(float *phase, float incr)
   
 void *dsp_init(struct world *w, int g)
 {
-  struct sinosc *s = malloc(sizeof(s));
+  struct sinosc *s = malloc(sizeof(struct sinosc));
   s->phase = 0.0;
-  w_p_set1(w, g, 0, 440.0);
-  w_p_set1(w, g, 1, 0.1);
-  w_p_set1(w, g, 2, 0.5);
+  s->f = 440.0;
+  s->a = 0.1;
+  s->p = 0.5;
+  w_p_set1(w, g, 0, s->f);
+  w_p_set1(w, g, 1, s->a);
+  w_p_set1(w, g, 2, s->p);
   return (void*)s;
 }
 
@@ -38,13 +41,25 @@ void dsp_step(struct world *w, int g, void *ptr, int nf)
 {
   int i;
   struct sinosc *s = (struct sinosc *)ptr;
+  float f = s->f;
+  float a = s->a;
+  float p = s->p;
+  float fe = w_p_get1(w, g, 0);
+  float ae = w_p_get1(w, g, 1);
+  float pe = w_p_get1(w, g, 2);
+  float fi = (fe - f) / (float)nf;
+  float ai = (ae - a) / (float)nf;
+  float pi = (pe - p) / (float)nf;
   for(i = 0; i < nf; i++) {
-    float f = w_p_get1(w, g, 0);
-    float a = w_p_get1(w, g, 1);
-    float p = w_p_get1(w, g, 2);
-    s->incr = hz_to_incr(f, w_sr(w));
-    step_phasor(&(s->phase), s->incr);
+    float incr = hz_to_incr(f, w_sr(w));
+    step_phasor(&(s->phase), incr);
     float n = sinf(s->phase) * a;
     w_out2(w, i, n * p, n * (1 - p));
+    f += fi;
+    a += ai;
+    p += pi;
   }
+  s->f = fe;
+  s->a = ae;
+  s->p = pe;
 }
