76e1978e603b4495de8acafcf94d43137bd96836
rd <rd@slavepianos.org>
2021-01-29 22:21:02 +0000

    ~dl,~w_in,~w_out

--- cmd/jack-dl.h
+++ cmd/jack-dl.h
@@ -30,9 +30,9 @@ struct world {
 #define w_sr(w) (w)->sr
 #define w_c_get1(w,i) (w)->ctl[(i)]
 #define w_c_set1(w,i,n) (w)->ctl[(i)]=(n)
-#define w_in1(w,i) (w)->in[0][(i)]
-#define w_out1(w,i,n) (w)->out[0][(i)]=(n)
-#define w_out2(w,i,n1,n2) {(w)->out[0][(i)]=(n1);(w)->out[1][(i)]=(n2);}
+#define w_in1(w,c,i) (w)->in[(c)][(i)]
+#define w_out1(w,c,i,n) (w)->out[(c)][(i)]=(n)
+#define w_out2(w,c,i,n1,n2) {(w)->out[(c)][(i)]=(n1);(w)->out[(c)+1][(i)]=(n2);}
 /* run-time check... */
 #define w_b_read1(w,b,i) (w)->bl[(b)] > (i) ? (w)->bd[(b)][(i)] : 0.0
 #define w_b_write1(w,b,i,n) if((w)->bl[(b)] > (i)) {(w)->bd[(b)][(i)]=(n);}
